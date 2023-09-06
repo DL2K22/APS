0da362b9dc064c3f70525a92d680565054eefbea
Lucas Barrios <lucas.barrios@scania.com>
2023-09-06 13:01:06 -0300

    Atualização

--- Java/Index.js
+++ Java/Index.js
@@ -131,19 +131,6 @@ function exibirGrafico() {
 /* -------------------------------------------------------------------------- */
 /*                                    SITE                                    */
 /* -------------------------------------------------------------------------- */
-window.onscroll = function() {myFunction()};
-
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
-
 const scrollButton = document.getElementById("scrollButton");
 
 // Função para rolar suavemente para a próxima sessão
